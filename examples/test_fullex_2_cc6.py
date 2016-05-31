c4d5b1672a8a1552552568540d5ecfb6bb679a6c
Angus yeh <a.yeh@auckland.ac.nz>
2016-05-31 16:27:01 +1200

    Example cc6 modified to suit linux users.

--- examples/test_fullex_2_cc6.py
+++ examples/test_fullex_2_cc6.py
@@ -90,12 +90,12 @@ t2geo_fn = "3DCoarsegrid.dat"
 t2dat_fn = "CC6C001.DAT"
 
 # this does not work in cygwin at the moment (no python vtk)
-# super_fn = t2_to_super(t2geo_fn,
-#                        t2dat_fn,
-#                        basepath=MODELDIR
-#                        )
+super_fn = t2_to_super(t2geo_fn,
+                       t2dat_fn,
+                       basepath=MODELDIR
+                       )
 # cheated by running in windows and get the files
-super_fn = 'CC6C001.json'
+# super_fn = 'CC6C001.json'
 
 # AUT2 uses dummy block for boundary condition, (atmospheric blocks here)
 # get rid of them to be identical to supermodel
@@ -108,7 +108,7 @@ mrun_t = T2ModelRun("aut2", t2dat_fn,
                     geo_filename=t2geo_fn,
                     ordering_map=map_out_atm,
                     fieldname_map=AUT2_FIELDMAP,
-                    simulator='AUTOUGH2_5Dbeta',
+                    # simulator='AUTOUGH2_5Dbeta',
                     basePath=os.path.realpath(MODELDIR)
                     )
 jrunner = SimpleJobRunner()
@@ -120,7 +120,7 @@ mres_t = jrunner.blockResult(mrun_t, jmeta)
 # construct supermodel run and benchamrk test
 mrun_s = SuperModelRun("super", super_fn,
                        fieldname_map=SUPER_FIELDMAP,
-                       simulator='supermodel.exe',
+                       # simulator='supermodel.exe',
                        basePath=os.path.realpath(MODELDIR)
                        )
 
