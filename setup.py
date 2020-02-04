7b113c9bee7e02452a10974520a6f413f9df8dd1
Adrian Croucher <a.croucher@auckland.ac.nz>
2020-02-04 14:16:18 +1300

    Add setup.py for installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import re
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="waiwera-credo",
+    version="2.0.0",
+    author="Waiwera Project",
+    author_email="waiwera.project@gmail.com",
+    description="Fork of CREDO computational model benchmarking toolkit, for use with the Waiwera geothermal flow simulator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://waiwera.github.io",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=2.7'
+)
